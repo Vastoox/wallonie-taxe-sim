5dcdffeef632342b21ef8bfdc2786da278ea19d8
Vastoox <oktay.03b@gmail.com>
2025-09-20 07:51:00 +0200

    Update index.js

--- backend/providers/index.js
+++ backend/providers/index.js
@@ -1,10 +1,9 @@
-
+// backend/providers/index.js
 const provider = process.env.PROVIDER || 'mock';
-if (provider === 'carquery') module.exports = require('./carquery');
-else if (provider === 'multi') module.exports = require('./multi');
-else if (provider === 'jato') module.exports = require('./jato');
-else if (provider === 'autovista') module.exports = require('./autovista');
-else if (provider === 'eurotax') module.exports = require('./eurotax');
-else module.exports = require('./mock');
-import * as rdw from "./rdw.js";
-export { rdw };
+if (provider === 'carquery')      module.exports = require('./carquery');
+else if (provider === 'multi')    module.exports = require('./multi');
+else if (provider === 'jato')     module.exports = require('./jato');
+else if (provider === 'autovista')module.exports = require('./autovista');
+else if (provider === 'eurotax')  module.exports = require('./eurotax');
+else if (provider === 'rdw')      module.exports = require('./rdw'); // ← AJOUT
+else                              module.exports = require('./mock');
