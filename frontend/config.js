11bc2a0605590b5dc0379ac2d52f18635bffe7f8
Vastoox <oktay.03b@gmail.com>
2025-09-21 02:58:39 +0200

    Update config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,2 +1,4 @@
 // frontend/config.js
 window.API_BASE = "https://wallonie-taxe-sim.onrender.com";
+// <- ce fichier est chargé par index.html AVANT ton script principal
+export const API_BASE = "https://wallonie-taxe-sim.onrender.com";
